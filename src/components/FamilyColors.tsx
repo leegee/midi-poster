de5a4f55d775bac1ed3e7ec9a473069568c5b7db
Lee Goddard <leegee@gmail.com>
2025-11-19 14:55:12 +0100

    simplify css

--- src/components/FamilyColors.tsx
+++ src/components/FamilyColors.tsx
@@ -7,38 +7,16 @@ export default function FamilyColorEditor() {
     const families = Object.keys(DEFAULT_FAMILY_COLOR);
 
     return (
-        <div style="display: flex; flex-direction: column; gap: 0.6em">
+        <section>
             <For each={families}>
                 {(family) => (
-                    <div style="display: flex; align-items: left; gap: 0.6em;">
-
-                        {/*
-                        <div
-                            style={`
-                            width: 2em;
-                            height: 1em;
-                            background: ${getFamilyColor(family)};
-                            border: 1px solid #000;
-                        `}
-                        />
-
-                        <label style="flex: 1;">{family}</label>
-
-                         <input
-                            type="color"
-                            value={getFamilyColor(family)}
-                            onInput={(e) => setFamilyColor(family, (e.target as HTMLInputElement).value)}
-                        /> */}
-
-                        <ColorPicker
-                            label={family}
-                            value={getFamilyColor(family)}
-                            onChange={(value: string) => setFamilyColor(family, value)}
-                        />
-
-                    </div>
+                    <ColorPicker
+                        label={family}
+                        value={getFamilyColor(family)}
+                        onChange={(value: string) => setFamilyColor(family, value)}
+                    />
                 )}
             </For>
-        </div>
+        </section>
     );
 }
